35857e5dcdb5f426e14228f269dc7a8805ea8cc8
Jeff Vega <jeff@intellibliss.com>
2018-02-22 13:22:40 -0800

    shopping list take 1

--- shopping-list.js
+++ shopping-list.js
@@ -1,13 +1,3 @@
 $(function() {
   
-  $("button").click(function(event) {
-    $("ul").append(
-      "<li>" +
-      ["cat", "dog", "rock"][Math.floor(Math.random()*3)] + "</li>"
-    );
-  });
-  
-  $('ul').on('click', 'li', function(event) {
-    this.remove();
-  });
 });
