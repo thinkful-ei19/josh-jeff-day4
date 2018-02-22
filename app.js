b1c7a99227664a7c443ac13703f59f7d341f06c1
Jeff Vega <jeff@intellibliss.com>
2018-02-22 09:52:15 -0800

    cats take 1

---
+++ app.js
@@ -0,0 +1,9 @@
+function catPhotosSelect(){
+  $('.thumbnail ').on('click', function(event) {
+    let targetCat = $(this).find('img').attr('src');
+    let targetAlt = $(this).find('img').attr('alt');
+
+    $('.hero img').attr('src',targetCat).attr('alt',targetAlt);
+}
+
+$(catPhotosSelect);
